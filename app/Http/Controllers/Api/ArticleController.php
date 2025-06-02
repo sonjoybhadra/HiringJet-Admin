5872f2d2387f153363d195a3980247e6073ee9f6
chayansamanta <work.chayan2020@gmail.com>
2025-06-02 23:13:10 +0530

    Update CMS page end point

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -7,15 +7,34 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use App\Models\Article;
+use App\Models\Page;
 
-class ArticleController extends BaseApiController
+class CmsArticleController extends BaseApiController
 {
+    public function getPage(Request $request)
+    {
+        $sql = Page::where('status', 1);
+        if(!empty($requesr->slug)){
+            $sql->where('page_slug', $request->slug);
+        }
+        $list = $sql->get();
+        return $this->sendResponse(
+                $list,
+                'CMS list.'
+            );
+    }
+
     public function getArticle(Request $request)
     {
-        $list = Article::where('status', 1)->get();
+        $sql = Article::where('status', 1);
+        if(!empty($requesr->slug)){
+            $sql->where('page_slug', $request->slug);
+        }
+        $list = $sql->get();
         return $this->sendResponse(
                 $list,
                 'Article list.'
             );
     }
+
 }
