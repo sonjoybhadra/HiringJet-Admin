5f9abab9e70463d59dd87bb51fe27842da1ca777
chayansamanta <work.chayan2020@gmail.com>
2025-06-17 23:12:41 +0530

    update job search response

--- app/Http/Controllers/Api/JobSearchController.php
+++ app/Http/Controllers/Api/JobSearchController.php
@@ -25,7 +25,9 @@ class JobSearchController extends BaseApiController
         try {
             $sql = PostJob::select('post_jobs.*')->where('job_type', $job_type);
             if(Auth::guard('api')->check()){
-                $sql->addSelect(DB::raw('(SELECT COUNT(*) FROM shortlisted_jobs WHERE shortlisted_jobs.user_id = '.Auth::guard('api')->user()->id.' and shortlisted_jobs.job_id = post_jobs.id and shortlisted_jobs.status=1) AS job_applied_status'));
+                $sql->addSelect(DB::raw('(SELECT COUNT(*) FROM post_job_user_applieds WHERE post_job_user_applieds.user_id = '.Auth::guard('api')->user()->id.' and post_job_user_applieds.job_id = post_jobs.id and post_job_user_applieds.status=1) AS job_applied_status'));
+
+                $sql->addSelect(DB::raw('(SELECT COUNT(*) FROM shortlisted_jobs WHERE shortlisted_jobs.user_id = '.Auth::guard('api')->user()->id.' and shortlisted_jobs.job_id = post_jobs.id and shortlisted_jobs.status=1) AS job_shortlisted_status'));
             }
 
             if(!empty($request->country)){
@@ -103,7 +105,9 @@ class JobSearchController extends BaseApiController
                         ->with('functionalArea')
                         ->with('experienceLevel');
             if(Auth::guard('api')->check()){
-                $sql->addSelect(DB::raw('(SELECT COUNT(*) FROM shortlisted_jobs WHERE shortlisted_jobs.user_id = '.Auth::guard('api')->user()->id.' and shortlisted_jobs.job_id = post_jobs.id and shortlisted_jobs.status=1) AS job_applied_status'));
+                $sql->addSelect(DB::raw('(SELECT COUNT(*) FROM post_job_user_applieds WHERE post_job_user_applieds.user_id = '.Auth::guard('api')->user()->id.' and post_job_user_applieds.job_id = post_jobs.id and post_job_user_applieds.status=1) AS job_applied_status'));
+
+                $sql->addSelect(DB::raw('(SELECT COUNT(*) FROM shortlisted_jobs WHERE shortlisted_jobs.user_id = '.Auth::guard('api')->user()->id.' and shortlisted_jobs.job_id = post_jobs.id and shortlisted_jobs.status=1) AS job_shortlisted_status'));
             }
             return $this->sendResponse(
                 $sql->first(),
