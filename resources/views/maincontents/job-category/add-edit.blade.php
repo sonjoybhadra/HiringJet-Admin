98edf901c300a6f3a44d1a8e8abb11ce360b7b01
subhomoy samanta <subhomoy@keylines.net>
2025-06-26 21:29:50 +0530

    role & admin user & signin bug fixing

--- resources/views/maincontents/job-category/add-edit.blade.php
+++ resources/views/maincontents/job-category/add-edit.blade.php
@@ -40,7 +40,7 @@ $controllerRoute = $module['controller_route'];
             }
             ?>
             <div class="card-body">
-                <form id="formAccountSettings" action="" method="POST" enctype="multipart/form-data">
+                <form id="formAccountSettings" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="row">
                         <div class="col-md-6">
