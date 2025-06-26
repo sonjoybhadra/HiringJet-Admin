401c61a2587218335417c4511a2b1d1a0c5fc406
subhomoy samanta <subhomoy@keylines.net>
2025-06-26 21:39:54 +0530

    role & admin user & signin bug fixing

--- app/Models/JobCategory.php
+++ app/Models/JobCategory.php
@@ -9,6 +9,8 @@ class JobCategory extends Model
 {
     use SoftDeletes;
 
+    protected $fillable = ['name', 'status']; // ✅ allow mass assignment
+
     public function postJobs()
     {
         return $this->hasMany(PostJob::class, 'job_category');
