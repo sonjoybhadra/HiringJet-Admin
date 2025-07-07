e162e260f6ede52c395707b4a18778b88fe117d7
chayansamanta <work.chayan2020@gmail.com>
2025-07-07 21:05:29 +0530

    Update count condition

--- app/Http/Controllers/Api/CommonController.php
+++ app/Http/Controllers/Api/CommonController.php
@@ -550,13 +550,29 @@ class CommonController extends BaseApiController
                                 ->where('status', 1)
                                 ->count();
 
+        // $list->posted_jobs_countries = DB::table('post_jobs')
+        //                                     ->selectRaw("countries.id as location_id, countries.name, COUNT(*) as total")
+        //                                     ->fromRaw("(SELECT jsonb_array_elements_text(location_countries::jsonb)::bigint as location_id FROM post_jobs) as job_locations")
+        //                                     ->join('countries', 'countries.id', '=', 'job_locations.location_id')
+        //                                     ->groupBy('countries.id', 'countries.name')
+        //                                     ->orderByDesc('total')
+        //                                     ->take(8)
+        //                                     ->get();
+
         $list->posted_jobs_countries = DB::table('post_jobs')
-                                            ->selectRaw("countries.id as location_id, countries.name, COUNT(*) as total")
-                                            ->fromRaw("(SELECT jsonb_array_elements_text(location_countries::jsonb)::bigint as location_id FROM post_jobs) as job_locations")
+                                            ->selectRaw('countries.id as location_id, countries.name, COUNT(*) as total')
+                                            ->fromRaw("
+                                                (
+                                                    SELECT jsonb_array_elements_text(location_countries::jsonb)::bigint as location_id
+                                                    FROM post_jobs
+                                                    WHERE location_countries IS NOT NULL
+                                                    AND jsonb_typeof(location_countries::jsonb) = 'array'
+                                                ) as job_locations
+                                            ")
                                             ->join('countries', 'countries.id', '=', 'job_locations.location_id')
                                             ->groupBy('countries.id', 'countries.name')
                                             ->orderByDesc('total')
-                                            ->take(8)
+                                            ->limit(8)
                                             ->get();
 
         $list->posted_jobs_category = PostJob::select('post_jobs.job_category', 'job_categories.name', DB::raw('COUNT(*) as total'))
