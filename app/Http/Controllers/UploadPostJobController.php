f129f3e2f62c04b75dbf14f3328e885959e81e39
subhomoy samanta <subhomoy@keylines.net>
2025-07-30 22:26:54 +0530

    upload jobs bug fixing

--- app/Http/Controllers/UploadPostJobController.php
+++ app/Http/Controllers/UploadPostJobController.php
@@ -106,7 +106,8 @@ class UploadPostJobController extends Controller
                             fclose($handle);
                         }
                     /* extract csv file */
-                    // Helper::pr($rows,0);
+                    Helper::pr($rows,0);
+                    echo count($rows);
                     /* insert data into post_jobs table */
                         if($rows){
                             for($k=0;$k<count($rows);$k++){
