9812cae3a5195fc62cedb0c7686c4d37ee7aafbe
subhomoy samanta <subhomoy@keylines.net>
2025-07-30 22:13:38 +0530

    upload jobs bug fixing

--- app/Http/Controllers/UploadPostJobController.php
+++ app/Http/Controllers/UploadPostJobController.php
@@ -386,7 +386,7 @@ class UploadPostJobController extends Controller
                                         'status'                    => 1,
                                         'upload_id'                 => $upload_id,
                                     ];
-                                    // Helper::pr($fields);
+                                    Helper::pr($fields);
 
                                     $maxId = DB::table('post_jobs')->max('id');
                                     DB::statement("ALTER SEQUENCE post_jobs_id_seq RESTART WITH " . ($maxId + 1));
