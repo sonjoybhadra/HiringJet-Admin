45b1bc9922dd5410685cf50fb3965d9ec0a811ef
Sonjoy Bhadra <sbhadra0@gmail.com>
2025-08-13 22:25:24 +0530

    chnage on EmployerPostJobRegistrationController

--- app/Http/Controllers/Api/Employer/EmployerPostJobRegistrationController.php
+++ app/Http/Controllers/Api/Employer/EmployerPostJobRegistrationController.php
@@ -150,6 +150,7 @@ public function postJobComplete(Request $request)
                 'landline' => $request->landline ?? '',
                 'employe_type' => $request->employe_type ?? 'company',
                 'web_url' => $request->web_url ?? '',
+                'no_of_employee' => $request->no_of_employee ?? 1,
                 'logo' => '',
                 'status' => 0
             ]);
