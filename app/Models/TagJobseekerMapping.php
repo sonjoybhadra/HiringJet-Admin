4cb41cbcb0fd58df9cacf8409bf1735997dc699b
chayansamanta <work.chayan2020@gmail.com>
2025-08-16 21:15:59 +0530

    Update tag jobseeker mapping

--- app/Models/TagJobseekerMapping.php
+++ app/Models/TagJobseekerMapping.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TagJobseekerMapping extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'tag_id',
+        'jobseeker_id'
+    ];
 }
