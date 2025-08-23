cfa202e4a198d95bf1c3b7281d94d690d41192bf
chayansamanta <work.chayan2020@gmail.com>
2025-08-23 11:18:19 +0530

    Update job draft model fillable

--- app/Models/EmployerPostJobDraft.php
+++ app/Models/EmployerPostJobDraft.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmployerPostJobDraft extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'job_no',
+        'position_name',
+        'request_json'
+    ];
 }
