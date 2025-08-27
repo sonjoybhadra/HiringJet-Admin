22f005d9581be65711b361288c6e660f7ec3f489
subhomoy samanta <subhomoy@keylines.net>
2025-08-27 20:21:23 +0530

    jobseeker different list

--- app/Http/Controllers/Common/TableController.php
+++ app/Http/Controllers/Common/TableController.php
@@ -11,7 +11,7 @@ class TableController extends Controller
 {
     public function fetch(Request $request)
     {
-        // Helper::pr($request->all());
+        Helper::pr($request->all());
         $table = $request->input('table');
         $routes = $request->input('routes');
         $orderBy = $request->input('orderBy', 'id');
@@ -66,8 +66,8 @@ class TableController extends Controller
             $pageName     = explode("/", $url);
             $pageSegment  = $pageName[0];
             $pageFunction = ((count($pageName)>1)?$pageName[1]:'');
-            echo '<pre>';print_r($pageName);
-            echo $pageFunction;die;
+            // echo '<pre>';print_r($pageName);
+            // echo $pageFunction;die;
             //if($pageFunction == 'profile-complete-list'){
                 $query->leftJoin('user_profile_completed_percentages', DB::raw("CAST($table.id AS TEXT)"), '=', DB::raw("CAST(user_profile_completed_percentages.user_id AS TEXT)"));
             //}
