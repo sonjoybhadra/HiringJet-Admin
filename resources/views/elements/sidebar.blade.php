9a51ebab2ddbdf2df54d499b001d335d0cc04630
subhomoy samanta <subhomoy@keylines.net>
2025-08-27 19:21:49 +0530

    jobseeker different list

--- resources/views/elements/sidebar.blade.php
+++ resources/views/elements/sidebar.blade.php
@@ -13,8 +13,7 @@ $pageSegment  = $pageName[0];
 $pageFunction = ((count($pageName)>1)?$pageName[1]:'');
 $user_id = session('user_id');
 $role_id = (($user)?$user->role_id:0);
-print_r($url);
-// print_r($pageName);
+
 ?>
 <div class="app-brand demo">
   <a href="<?=url('/dashboard')?>" class="app-brand-link">
@@ -549,11 +548,12 @@ print_r($url);
           </a>
         </li>
         <?php
+        $url_break      = explode("profile-complete-list/", $url);
+        $pageParam      = $url_break[1];
         $profile_completes = ProfileComplete::select('id', 'name')->where('status', '=', 1)->orderBy('id', 'ASC')->get();
-        echo $pageFunction;
         if($profile_completes){ foreach($profile_completes as $profile_complete){
         ?>
-          <li class="menu-item <?=(($pageSegment == 'jobseeker' && $pageFunction == 'list')?'active':'')?>">
+          <li class="menu-item <?=(($pageSegment == 'jobseeker' && $pageFunction == 'profile-complete-list' && $pageParam == Helper::encoded($profile_complete->id))?'active':'')?>">
             <a href="<?=url('/jobseeker/profile-complete-list/' . Helper::encoded($profile_complete->id))?>" class="menu-link">
               <div data-i18n="<?=$profile_complete->name?>"><i class="fa-solid fa-arrow-right"></i> <?=$profile_complete->name?></div>
             </a>
