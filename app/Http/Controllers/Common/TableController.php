bde8f0de33339f8e4ee9a5be8150e7f70bdf5a05
subhomoy samanta <subhomoy@keylines.net>
2025-08-27 20:02:47 +0530

    jobseeker different list

--- app/Http/Controllers/Common/TableController.php
+++ app/Http/Controllers/Common/TableController.php
@@ -176,7 +176,7 @@ class TableController extends Controller
             } else {
                 $pageParam      = '';
             }
-            echo $pageParam;
+            
             $query->where('user_profile_completed_percentages.profile_completes_id', '=', $pageParam);
         }
         
