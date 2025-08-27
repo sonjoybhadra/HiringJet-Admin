1bd6ec9be48abe2f495bf173dc313b6d23c8d6db
subhomoy samanta <subhomoy@keylines.net>
2025-08-27 20:11:45 +0530

    jobseeker different list

--- app/Http/Controllers/Common/TableController.php
+++ app/Http/Controllers/Common/TableController.php
@@ -192,7 +192,7 @@ class TableController extends Controller
             });
         }
 
-        DB::enableQueryLog();
+        // DB::enableQueryLog();
 
         // Count before pagination
         $total = (clone $query)->count();
@@ -208,10 +208,9 @@ class TableController extends Controller
             });
 
         // Get last query
-        $queries = DB::getQueryLog();
-        $lastQuery = end($queries);
-
-        print_r($lastQuery);
+        // $queries = DB::getQueryLog();
+        // $lastQuery = end($queries);
+        // print_r($lastQuery);
 
         return response()->json([
             'data' => $data,
