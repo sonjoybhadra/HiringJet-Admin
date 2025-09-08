025c53ee94d3c54019f2a538286d225ae1297a7c
subhomoy samanta <subhomoy@keylines.net>
2025-09-08 19:42:33 +0530

    jobseeker module updated

--- public/material/backend/assets/js/form-wizard-validation.js
+++ public/material/backend/assets/js/form-wizard-validation.js
@@ -120,6 +120,13 @@
             }
           }
         },
+        functional_area: {
+          validators: {
+            notEmpty: {
+              message: 'Functional Area is required'
+            }
+          }
+        },
         min_exp_year: {
           validators: {
             notEmpty: {
