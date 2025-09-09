eab35a3454a9b0299b9ab6ef9a8b6ef860d167af
chayansamanta <work.chayan2020@gmail.com>
2025-09-09 18:42:02 +0530

    Update user resume model fillable property

--- app/Models/UserResume.php
+++ app/Models/UserResume.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserResume extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['user_id', 'cv', 'is_default'];
 }
