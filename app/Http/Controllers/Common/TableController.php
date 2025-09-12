dc59cedd965bd8ca03fb59e57557b69353277975
subhomoy samanta <subhomoy@keylines.net>
2025-09-12 20:55:37 +0530

    employer homepage update

--- app/Http/Controllers/Common/TableController.php
+++ app/Http/Controllers/Common/TableController.php
@@ -70,6 +70,7 @@ class TableController extends Controller
         if ($table === 'post_jobs') {
             $query->leftJoin('users', DB::raw("CAST($table.created_by AS TEXT)"), '=', DB::raw("CAST(users.id AS TEXT)"));
             $query->leftJoin('employers', DB::raw("CAST($table.employer_id AS TEXT)"), '=', DB::raw("CAST(employers.id AS TEXT)"));
+            $query->leftJoin('user_employers', DB::raw("CAST(employers.id AS TEXT)"), '=', DB::raw("CAST(user_employers.business_id AS TEXT)"));
         }
         if ($table === 'contact_us') {
             $query->leftJoin('cities', DB::raw("CAST($table.city_id AS TEXT)"), '=', DB::raw("CAST(cities.id AS TEXT)"));
@@ -134,7 +135,7 @@ class TableController extends Controller
                 //     return 'employers.name as employer_name';
                 // }
                 if ($col === 'employer_id') {
-                    return "$table.employer_id"; // keep raw employer_id
+                    return "user_employers.id as employer_id"; // keep raw employer_id
                 }
                 // return 'users.first_name as created_by_name';
             }
