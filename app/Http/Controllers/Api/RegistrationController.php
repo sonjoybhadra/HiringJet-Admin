72731b35c850844215e0b44c873eb87f1d6aa7f4
chayansamanta <work.chayan2020@gmail.com>
2025-09-16 20:20:33 +0530

    Enable debugg point for jobseeker setup profile

--- app/Http/Controllers/Api/RegistrationController.php
+++ app/Http/Controllers/Api/RegistrationController.php
@@ -281,7 +281,7 @@ class RegistrationController extends BaseApiController
             return $this->sendError('Validation Error', $validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
-        try{
+        // try{
             $image_path = "";
             if (request()->hasFile('profile_image')) {
                 $file = request()->file('profile_image');
@@ -380,9 +380,9 @@ class RegistrationController extends BaseApiController
 
             return $this->sendResponse($this->getUserDetails(), 'Setup profile has done. Please complete your profile now.');
 
-        } catch (\Exception $e) {
-            return $this->sendError('Error', 'Sorry!! Unable to complete setup profile.');
-        }
+        // } catch (\Exception $e) {
+        //     return $this->sendError('Error', 'Sorry!! Unable to complete setup profile.');
+        // }
     }
 
     public function completeProfile(Request $request, User $user)
