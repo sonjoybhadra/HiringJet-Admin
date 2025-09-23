1a975ee84a6b14a83ee9a1d821b33ddb195397b5
subhomoy samanta <subhomoy@keylines.net>
2025-09-23 21:39:01 +0530

    post job other designation bug fixing

--- routes/web.php
+++ routes/web.php
@@ -69,7 +69,7 @@ Route::get('/test-email-function', [AuthController::class, 'showEmailTestPage'])
 Route::post('/test-email-function', [AuthController::class, 'testEmailFunction']);
 
 // post jobs designation update
-Route::post('/postjob-other-designation-update', [AuthController::class, 'postjobOtherDesignationUpdate']);
+Route::get('/postjob-other-designation-update', [AuthController::class, 'postjobOtherDesignationUpdate']);
 
 Route::get('/', [AuthController::class, 'showLogin'])->name('login');
 Route::post('signin', [AuthController::class, 'login'])->name('signin');
