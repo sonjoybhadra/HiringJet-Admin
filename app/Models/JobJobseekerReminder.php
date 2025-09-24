4de6fae56b287124df65882b257ad7e1dccf1d89
chayansamanta <work.chayan2020@gmail.com>
2025-09-24 23:18:05 +0530

    Update reminder model fillable

--- app/Models/JobJobseekerReminder.php
+++ app/Models/JobJobseekerReminder.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobJobseekerReminder extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['job_id', 'jobseeker_id', 'employer_id', 'reminder_count'];
 }
